7b47c1cfa7fa7dfdd1aa8109f6fc06ce8bd71009
samwhelp <samwhelp@users.noreply.github.com>
2016-09-22 15:59:12 +0800

    refactoring

--- v-class/cin.js
+++ v-class/cin.js
@@ -1053,14 +1053,27 @@
 
 				},
 
-
-				orderBy_Phonetic: function(a, b) {
+				orderBy_PhoneticKeySeq: function(a, b) {
 					//console.log(a);
 					//console.log(a.phonetic_keyseq.localeCompare(b.phonetic_keyseq));
 					//return a.phonetic_keyseq.localeCompare(b.phonetic_keyseq);
 
 					var aa = a.phonetic_keyseq + '-' + a.grp + '-' + a.cns;
 					var bb = b.phonetic_keyseq + '-' + b.grp + '-' + b.cns;
+
+					return aa.localeCompare(bb);
+
+					//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/localeCompare
+					//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+				},
+
+				orderBy_Phonetic: function(a, b) {
+					//console.log(a);
+					//console.log(a.phonetic.localeCompare(b.phonetic));
+					//return a.phonetic.localeCompare(b.phonetic);
+
+					var aa = a.phonetic + '-' + a.grp + '-' + a.cns;
+					var bb = b.phonetic + '-' + b.grp + '-' + b.cns;
 
 					return aa.localeCompare(bb);
 
@@ -1080,7 +1093,7 @@
 					csv += this.makLine_Csv_Head();
 
 
-					this._PhoneticList.sort(this.orderBy_Phonetic);
+					this._PhoneticList.sort(this.orderBy_PhoneticKeySeq);
 
 					for (var item of this._PhoneticList) {
 						char_def += this.makLine_CharDef(item);
